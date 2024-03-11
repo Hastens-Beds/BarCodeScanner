6e9ef64096d37a4a24c3100f567ac4da4a20827d
Atikur Rahman <atik.bd08@gmail.com>
2024-03-11 14:46:59 +0600

    update package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CodeScannerPackage",
+    name: "BarCodeScanner",
     platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
