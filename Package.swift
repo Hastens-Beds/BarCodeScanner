6d71fc004d8a893ac627957fd53377267e147651
Atikur Rahman <atik.bd08@gmail.com>
2024-03-11 14:40:58 +0600

    rename package to BarCodeScanner

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CodeScannerPackage",
-            targets: ["CodeScannerPackage"]),
+            name: "BarCodeScanner",
+            targets: ["BarCodeScanner"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,11 +20,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CodeScannerPackage",
+            name: "BarCodeScanner",
             dependencies: [],
             path: "Sources/"),
         .testTarget(
-            name: "CodeScannerPackageTests",
-            dependencies: ["CodeScannerPackage"]),
+            name: "BarCodeScannerTests",
+            dependencies: ["BarCodeScanner"]),
     ]
 )
